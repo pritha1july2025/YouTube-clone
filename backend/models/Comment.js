3c1cd68a48445c190d921ea80953389aea78fa51
pritha1july2025 <selvparitha5598@gmail.com>
2026-01-13 22:56:07 +0530

    Backend: Comment: Comment schema is present

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -3,10 +3,11 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema(
   {
     videoId: { type: mongoose.Schema.Types.ObjectId, ref: "Video", required: true },
+
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
     text: { type: String, required: true }
-  },
-  { timestamps: true }
+  },  { timestamps: true }
 );
 
 export default mongoose.model("Comment", commentSchema);
