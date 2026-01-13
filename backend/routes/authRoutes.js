42bdb6513c01cae02892d5c74c0e24c61ddf2037
pritha1july2025 <selvparitha5598@gmail.com>
2026-01-13 23:01:08 +0530

    Backend:routes:authroutes for register and login routes also the secret key is present

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,24 +13,19 @@ const JWT_SECRET = "$$Secret-123##"; //SECRET KEY
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
-
     if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
-
     const hashed = await bcrypt.hash(password, 10);
-
     const user = await User.create({
       username,
       email,
       password: hashed
     });
-
     res.status(201).json({
       message: "User registered successfully",
       user
